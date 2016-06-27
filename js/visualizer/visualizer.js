833acf249dd0147b1350f6e8a640bcf53fa285bc
yishnish <yishnish@gmail.com>
2016-06-27 18:00:17 -0400

    starting to build out the visualizer as a thing that manages converting world data into html and updating each world node as the data changes

--- js/visualizer/visualizer.js
+++ js/visualizer/visualizer.js
@@ -1,1 +1,59 @@
-function Visualizer(){ }
+/**
+ maybe visualizer does something like take a 2d array of objects that have
+ an attribute that is like "iAmA" which says they are a type of creature
+ then visualizer colors all the cells with their corresponding color
+ maybe color comes from a color mapping somewhere
+
+ */
+function Visualizer(world, colorMapping) {
+    var nodeStuff = createNodeStuff();
+    var nodes = nodeStuff.nodeCollection;
+    var nodeHTML = nodeStuff.nodeHTML;
+
+    validateDataShape();
+
+    this.thingAt = function (x, y) {
+        return {color: null};
+    };
+
+    this.update = function () {
+    };
+
+    this.getDisplay = function () {
+        return {rows: world.length, columns: world[0].length};
+    };
+
+//private
+
+    function createNodeStuff(){
+        var tableNode  = document.createElement("table");
+        var nodeCollection = [];
+
+        world.forEach(function (row) {
+            var r = [];
+            var rowNode = document.createElement("tr");
+            row.forEach(function (col) {
+                var tdNode = document.createElement("td");
+                tdNode.setAttribute("color", colorMapping[col.iAmA]);
+                rowNode.appendChild(tdNode);
+                r.push(tdNode)
+            });
+            tableNode.appendChild(rowNode);
+            nodeCollection.push(r);
+        });
+
+        return {nodeHTML: tableNode, nodeCollection: nodeCollection};
+    }
+
+    function validateDataShape() {
+        var rowCount = world.length;
+        if (rowCount > 0) {
+            var colCount = world[0].length;
+            world.forEach(function (row) {
+                if (row.length !== colCount) {
+                    throw new Error("Non rectangular world");
+                }
+            });
+        }
+    }
+}
--- test/js/visualizerSpec.js
+++ test/js/visualizerSpec.js
@@ -1,6 +1,54 @@
-describe('visualizers', function () {
-    it('should exist', function () {
-        var viz = new Visualizer();
-        expect(viz).toBeDefined();
-    })
+describe('Visualizers', function () {
+    var colorMapping = {"vole": "brown", "bird" : "blue"};
+
+    describe('building a visualization', function () {
+        describe("from a data array", function () {
+            describe("when there is no data", function () {
+                it('should be ok', function () {
+                    expect(function(){new Visualizer([], colorMapping)}).not.toThrow();
+                })
+            });
+            describe("when there is data", function () {
+                describe("when the data isn't rectangular", function () {
+                    it("should fail", function () {
+                        expect(function () {
+                            new Visualizer(
+                                [
+                                    [{iAmA: "vole"}, {iAmA: "vole"}],
+                                    [{iAmA: "vole"}]
+                                ]
+                            , colorMapping)
+                        }).toThrowError("Non rectangular world");
+                    });
+                });
+                describe("when the data is rectangular", function () {
+                    it('should create a DOM table with the same number of rows and columns as the data', function () {
+                        var viz = new Visualizer([[{iAmA: "vole"}]], colorMapping);
+                        expect(viz.getDisplay().rows).toEqual(1);
+                        expect(viz.getDisplay().columns).toEqual(1);
+                    })
+                });
+                describe('when there is a color mapping', function(){
+                    xit('table elements should be colored according to their matching type', function(){
+                        var viz = new Visualizer([[{iAmA: "vole"}, {iAmA: "bird"}]], colorMapping);
+                        expect(viz.thingAt(0, 0).color).toBe("brown");
+                        expect(viz.thingAt(0, 1).color).toBe("blue");
+                    })
+                })
+            });
+        });
+        describe("updating a display", function () {
+            describe("when there are no underlying data elements", function () {
+                var viz;
+                beforeEach(function () {
+                    var myWorld = [];
+                    viz = new Visualizer(myWorld, colorMapping);
+                });
+                xit('should not update anything', function () {
+                    viz.update();
+                    expect(viz.thingAt(0,0).color).toBe("transparent");
+                });
+            });
+        });
+    });
 });
