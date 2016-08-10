4039ae39103d3dfd9d99b7cf7885a060b59e4ede
yishnish <yishnish@gmail.com>
2016-08-10 18:21:36 +0000

    porking in separate module now.

--- test/js/things/copulatesSpec.js
+++ test/js/things/copulatesSpec.js
@@ -1,6 +1,9 @@
 describe("things that copulate to reproduce", function () {
-    it('should only try to hump things of the same type', function () {
+    beforeAll(function () {
         Thing.prototype.mixin(Copulates);
+    });
+
+    it('should only try to hump things of the same type', function () {
         var thing = new Thing('whatever');
         var fruti = new FruitBush();
         var world = new World([
@@ -16,5 +19,40 @@ describe("things that copulate to reproduce", function () {
         world.add(new Thing('whatever'), new Coordinates(0, 1));
         thing.tryHumping(turn);
         expect(thing.hump).toHaveBeenCalled();
+    });
+
+    it('should only look in adjacent squares for copulatees', function () {
+        var thing = new Thing('whatever');
+
+        var world = new World([
+            [new Thing('whatever'), new Thing('whatever'), new Thing('whatever'), new Thing('whatever'), new Thing('whatever')],
+            [new Thing('whatever'), null,                   null,                   null,                new Thing('whatever')],
+            [new Thing('whatever'), null,                   thing,                  null,                new Thing('whatever')],
+            [new Thing('whatever'), null,                   null,                   null,                new Thing('whatever')],
+            [new Thing('whatever'), new Thing('whatever'), new Thing('whatever'), new Thing('whatever'), new Thing('whatever')]
+        ]);
+
+        spyOn(thing, 'hump');
+        var turn = new TurnContext(world, thing, new Coordinates(2, 2));
+        thing.tryHumping(turn);
+        expect(thing.hump).not.toHaveBeenCalled();
+    });
+
+    describe('getting pregnant', function () {
+        it('should get pregnant only if vitality is maxed out', function () {
+            var thing1 = new Thing('whatever');
+            var thing2 = new Thing('whatever');
+            var world = new World([
+                [thing1, thing2]
+            ]);
+            var turn = new TurnContext(world, thing1, new Coordinates(0, 0));
+            thing2.vitality = thing2.MAX_VITALITY - 1;
+            thing1.tryHumping(turn);
+            expect(thing2.pregnant).toBeFalsy();
+
+            thing2.vitality = thing2.MAX_VITALITY;
+            thing1.tryHumping(turn);
+            expect(thing2.pregnant).toBe(true);
+        });
     });
 });
