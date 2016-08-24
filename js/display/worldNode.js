691605c35ca4da955c0cf639593060e190643b83
yishnish <yishnish@gmail.com>
2016-08-24 18:39:32 +0000

    refactor

--- js/display/worldNode.js
+++ js/display/worldNode.js
@@ -1,7 +1,9 @@
 function WorldNode(colorMapping, thing){
     var td = document.createElement("td");
     var thingDetails = new ThingDetails(thing);
+
     setUpStatsDisplay(td);
+
     this.color = function () {
         return td.getAttribute("color");
     };
@@ -17,33 +19,44 @@ function WorldNode(colorMapping, thing){
     this.setContents(thing);
 
     function setUpStatsDisplay(td) {
-        var stats;
-        td.onmouseover = function () {
-            td.classList.add('highlight');
-        };
-        td.onmousedown = function (event) {
-            if(event.button === 0) {
-                stats = thingDetails.display();
-                td.appendChild(stats);
-            }else if(event.button === 2){
-                td.classList.toggle('temp-highlight');
-                return false;
-            }
-            return false;
-        };
-        document.oncontextmenu = function(event) {
-            return false;
-        };
-        td.onmouseout = function () {
-            if(stats) {
-                td.removeChild(stats);
-                stats = null;
+        preventDefaultContextMenu();
+        hilightOnMouseover();
+        showStatsOnClick();
 
-            }
-            var highlighted = document.getElementsByClassName('highlight');
-            for(let hilited of highlighted) {
-                hilited.classList.remove('highlight');
-            }
-        };
+        function showStatsOnClick() {
+            td.onmousedown = function (event) {
+                if (event.button === 0) {
+                    stats = thingDetails.display();
+                    td.appendChild(stats);
+                } else if (event.button === 2) {
+                    td.classList.toggle('temp-highlight');
+                }
+                return false;
+            };
+        }
+
+        function hilightOnMouseover() {
+            var stats;
+            td.onmouseover = function () {
+                td.classList.add('highlight');
+            };
+
+            td.onmouseout = function () {
+                if(stats) {
+                    td.removeChild(stats);
+                    stats = null;
+
+                }
+                var highlighted = document.getElementsByClassName('highlight');
+                for(let hilited of highlighted) {
+                    hilited.classList.remove('highlight');
+                }
+            };
+        }
+        function preventDefaultContextMenu() {
+            document.oncontextmenu = function () {
+                return false;
+            };
+        }
     }
 }
