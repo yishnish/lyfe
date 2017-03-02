7017c8948dddacc5f7b37f221d6e262246eff15a
yishnish <yishnish@gmail.com>
2017-03-02 17:37:15 +0000

    added a karma code coverage tool

--- karma.conf.js
+++ karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function (config) {
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
-            'js/**/*.js': ['jshint'],
+            'js/**/*.js': ['jshint', 'coverage'],
             'test/js/**/*.js': ['jshint']
         },
 
@@ -41,7 +41,7 @@ module.exports = function (config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: ['progress', 'coverage'],
 
 
         // web server port
@@ -72,6 +72,11 @@ module.exports = function (config) {
 
         // Concurrency level
         // how many browser should be started simultaneous
-        concurrency: Infinity
+        concurrency: Infinity,
+
+        coverageReporter: {
+            type : 'html',
+            dir : 'coverage/'
+        }
     })
 }
